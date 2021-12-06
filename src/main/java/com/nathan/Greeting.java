59615e7085471f00b26cbe72b09e9cb9d4e4c5f7
nesty-juramir <nathan.nariai@gmail.com>
2021-12-06 13:32:20 -0300

    First Commit

---
+++ src/main/java/com/nathan/Greeting.java
@@ -0,0 +1,18 @@
+package com.nathan;
+
+public class Greeting {
+
+	private final Long id;
+	private final String content;
+	
+	public Long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public Greeting(Long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+}
---
+++ src/test/java/com/nathan/ApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
